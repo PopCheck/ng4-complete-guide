164f81ad283714384c30231739a3864b54a90e97
Pop Checker <popchecker123@gmail.com>
2022-01-20 18:08:36 +0530

    modules seggretated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,29 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipiesResolverService } from "./recipes/recipes-resolver.service";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent,
-        canActivate: [AuthGuard],
-        children: [
-            { path: '', component: RecipeStartComponent },
-            { path: 'new', component: RecipeEditComponent },
-            { path: ':id', component: RecipeDetailComponent, resolve: [ RecipiesResolverService] },
-            { path: ':id/edit', component: RecipeEditComponent, resolve: [ RecipiesResolverService] }
-    ]},
-    { path: 'shopping-list', component: ShoppingListComponent},
-    { path: 'auth', component: AuthComponent}
-
+    { 
+        path: 'recipes',
+        loadChildren: () => import('./recipes/recipes.module')
+            .then(x => x.RecipesModule) 
+    },
+    {
+        path: 'shopping-list',
+        loadChildren: () => import('./shopping-list/shopping-list.module')
+            .then(x => x.ShoppingListModule)
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module')
+            .then(x => x.AuthModule)
+    }
 ]
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, {
+        preloadingStrategy: PreloadAllModules
+    })],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
